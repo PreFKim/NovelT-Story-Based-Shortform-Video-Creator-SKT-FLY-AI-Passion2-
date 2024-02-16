8c694b5e9c37f15324dfa1ea3f3e2667f4e1e5db
PreFKim <pre_f_86@naver.com>
2024-02-16 23:17:33 +0900

    Update blending.py

--- src/transition/blending.py
+++ src/transition/blending.py
@@ -16,9 +16,13 @@ class Blending(Transition):
             alpha = 0
             transition = full(inputs[0].shape,self.num_frames,fps=inputs[0].fps)
             for j in range(self.num_frames):
-                transition.video[j] = cv2.addWeighted(inputs[i-1].video[-(j+1)],1-alpha,inputs[i].video[j],alpha,self.gamma)
+                transition.video[j] = cv2.addWeighted(inputs[i-1].video[-1],1-alpha,inputs[i].video[0],alpha,self.gamma)
                 alpha = alpha + d
             ret = ret + transition + inputs[i]
-
-
+            # 부드러운 연계
+            # for j in range(self.num_frames):
+            #     transition.video[j] = cv2.addWeighted(inputs[i-1].video[-(self.num_frames+j)],1-alpha,inputs[i].video[j],alpha,self.gamma)
+            #     alpha = alpha + d
+            # ret = ret[:-(self.num_frames)] + transition + inputs[i][self.num_frames:]
+            
         return ret
