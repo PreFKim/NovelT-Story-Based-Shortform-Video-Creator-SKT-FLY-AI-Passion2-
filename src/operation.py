ef395f1d331dadb962ffa4a187bde0949d8337a6
PreFKim <pre_f_86@naver.com>
2024-02-16 19:51:08 +0900

    Update operation.py (default range)
    - "put_text" function : set the default range

--- src/operation.py
+++ src/operation.py
@@ -20,22 +20,33 @@ def read_video(path:str):
     return Video(video=video,fps=fps)
     
 
-def put_text(video:Video,text:str,start_frame:int,end_frame:int,fontsize=12,color:tuple=(0,0,0),center_xy:tuple=None):
+def put_text(video:Video,text:str,start_frame:int=0,end_frame:int=None,fontsize=12,color:tuple=(0,0,0),center_xy:tuple=None,bg_color:tuple=None):
     ret = video.copy()
     h,w = video.shape
 
+    start_frame = 0 if start_frame is None else start_frame
+    end_frame = len(video) if end_frame is None else end_frame
+    
+    if start_frame<0: start_frame = max(start_frame + len(video),0)
+    if end_frame<0: end_frame = max(end_frame + len(video),0)
+
+    start_frame = min(len(video),start_frame)
+    end_frame = min(len(video),end_frame)
+
     if center_xy is None:
-        x1, y1 = w/2,h/2
+        x, y = w/2,h/2
     else :
-        x1, y1 = center_xy
+        x, y = center_xy
 
     for i in range(start_frame,end_frame):
         img_pillow = Image.fromarray(ret.video[i]).convert('RGB')
-
-        fontpath = "./fonts/gulim.ttc"
+        fontpath = "./fonts/Pretendard-Light.ttf"
         font = ImageFont.truetype(fontpath, fontsize)
         draw = ImageDraw.Draw(img_pillow, 'RGB')
-        draw.text([x1, y1],text=text, font=font, anchor='mm', fill=(color[2],color[1],color[0]))
+        if bg_color is not None:
+            left, top, right, bottom = draw.textbbox((x, y), text, font=font,anchor='mm')
+            draw.rectangle((left-fontsize/5, top-fontsize/5, right+fontsize/5, bottom+fontsize/5), fill=(bg_color[2],bg_color[1],bg_color[0]))
+        draw.text((x, y),text=text, font=font, anchor='mm', fill=(color[2],color[1],color[0]))
 
         ret.video[i] = np.array(img_pillow,dtype=np.uint8)
     return ret
