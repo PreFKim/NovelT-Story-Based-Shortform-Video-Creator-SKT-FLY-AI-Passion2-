4aa99e658e51a8a8baae6382b5756a14cdc0aed9
PreFKim <pre_f_86@naver.com>
2024-02-18 22:18:16 +0900

    Update subtitle.py ( Add Narration effect)

--- src/effect/subtitle.py
+++ src/effect/subtitle.py
@@ -1,6 +1,6 @@
 from .effect import Effect
 from ..video import Video
-from ..operation import put_text
+from ..operation import put_text, full
 
 class Subtitle(Effect):
     def __init__(self,text, start_frame:int=0, end_frame:int=None, fontsize:int=None, color=(0,0,0), h_ratio:float =0.7, bg_color=(255,255,255)):
@@ -22,4 +22,23 @@ class Subtitle(Effect):
         
         return put_text(video,self.text,self.start_frame,self.end_frame,fontsize=fontsize,color=self.color,center_xy=(w/2,h*self.h_ratio),bg_color=self.bg_color)
 
-    
+class Narration(Effect):
+    def __init__(self,text, start_frame:int=0, end_frame:int=None, fontsize:int=None, color=(255,255,255), bg_color=(0,0,0)):
+        super(Narration,self).__init__(start_frame=start_frame,end_frame=end_frame)
+        self.text = text
+        self.color = color
+        self.bg_color = bg_color
+        self.fontsize = fontsize
+        
+    def effect(self,video:Video):
+
+        h, w = video.shape
+
+        if self.fontsize is None:
+            fontsize = w//16
+        else :
+            fontsize = self.fontsize
+
+        video[self.start_frame:self.end_frame] = put_text(full(video.shape,self.end_frame-self.start_frame,color=self.bg_color),self.text,fontsize=fontsize,color=self.color)
+        
+        return video
