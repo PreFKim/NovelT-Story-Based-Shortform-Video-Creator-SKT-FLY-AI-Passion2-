d3aaa9e2ca59a0908b88626b0ca3a986d754213a
PreFKim <pre_f_86@naver.com>
2024-02-28 14:09:35 +0900

    Add config.py

--- config.py
+++ config.py
@@ -1,12 +1,10 @@
-
 # OpenAI API
-api_key = ""# Your OpenAI API Key
+api_key = "" # Your OpenAI API Key
 
 model="gpt-4-0125-preview"
 temperature=1
 max_tokens=2048
 top_p=1
-
 
 # Clova voice API
 client_id = "" # Your Clova API ID
